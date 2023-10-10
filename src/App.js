961858a6c3c7068484f61db6013c51ff3a44fe6a
aimen114 <aimenzafar095@gmail.com>
2023-10-10 18:34:58 +0500

    added credentials

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ export default function App() {
       <main>
         <Dictionary />
       </main>
+      <footer>
+        This project is created by{" "}
+        <a
+          href="https://unrivaled-gingersnap-27115c.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aimen Zafar
+        </a>
+        , Open sourced on{" "}
+        <a
+          href="https://github.com/aimen114/react-dictionary-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github{" "}
+        </a>
+        and hosted on{" "}
+        <a
+          href="https://quiet-pie-363727.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
